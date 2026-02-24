c067c96afe09f490bba04220449daa7223f708bd
juancho-create <jgallego.hq@gmail.com>
2026-02-24 14:39:24 -0500

    redi deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/CCT-website/',
     build: {
         rollupOptions: {
             input: {
